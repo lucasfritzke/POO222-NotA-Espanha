ae4dc72dba513ea920930cc22ede30776e5aa751
Lucas Fritzke <86270203+lucasfritzke@users.noreply.github.com>
2022-11-08 21:07:31 -0300

    Criando a package

---
+++ POO222NotAEspanha/src/moduleEspanha/Espanha.java
@@ -0,0 +1,72 @@
+package moduleEspanha;
+import java.awt.Image;
+import java.nio.file.Path;
+
+import fifa.NationalTeamInfos;
+import fifa.NationalTeamStats;
+
+public class Espanha implements NationalTeamInfos{
+
+	@Override
+	public int getHowManyMembers() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getOldestPlayer() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getYoungestPlayer() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getAverageAge() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getPlayer(int number) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getPressOfficerContacts() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getCountryName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Image getFlagImage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Path getTechnicalCommittee() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public NationalTeamStats getStatsResponsible() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+
+}
