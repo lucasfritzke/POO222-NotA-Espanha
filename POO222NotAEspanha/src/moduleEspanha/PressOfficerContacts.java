09b59b6f7a7482b7548512ca2530f2c754315ef0
preguicin <87340302+preguicin@users.noreply.github.com>
2022-11-20 19:15:48 -0300

    Implementando Technical Comittee
    Também corrigindo a implementação errada de technical comittee em press officer, além de implementar a mesma agora corretamente

--- POO222NotAEspanha/src/moduleEspanha/PressOfficerContacts.java
+++ POO222NotAEspanha/src/moduleEspanha/PressOfficerContacts.java
@@ -4,47 +4,58 @@ import java.io.Serializable;
 import java.time.LocalDate;
 
 public class PressOfficerContacts extends Person implements Serializable{
-	//Atributos 
-	private String nickname;
-	private String role;
-	private LocalDate birthDate;
+	//Atributes
+	private String tel1;
+	private String tel2;
+	private String  emailAccount;
+	private boolean manager;
 	
 	//Constructors
 	public PressOfficerContacts() {
 		super();
 	}
-	public PressOfficerContacts(String name,String nickname, String role, LocalDate birthDate) {
+	public PressOfficerContacts(String name,String tel1, String tel2, String email, boolean isManager) {
 		super(name);
-		setBirthDate(birthDate);
-		setName(nickname);
-		setRole(role);
+		setTel1(tel1);
+		setTel2(tel2);
+		setEmailAccount(email);
+		setManager(isManager);
+	
 	}
 	
-	//Nickname
-	public String getNickname() {
-		return nickname;
+	//Tel1
+	public String getTel1() {
+		return tel1;
 	}
-	public void setNickname(String nickname) {
-		if(nickname != null && !nickname.isBlank()) {this.nickname = nickname;}
-		else {throw new IllegalArgumentException("invalid nickname");}
+	public void setTel1(String tel1) {
+		if(tel1 != null && !tel1.isBlank()) {this.tel1 = tel1;}
+		else {throw new IllegalArgumentException("invalid tel1");}
 	}
-	//Role 
-	public String getRole() {
-		return role;
+	//Tel2
+	public String getTel2() {
+		return tel2;
 	}
-	public void setRole(String role) {
-		if(role != null && !role.isBlank()) {this.role = role;}
-		else {throw new IllegalArgumentException("invalid role");}
-		
+	public void setTel2(String tel2) {
+		if(tel2 != null && !tel2.isBlank()) {this.tel2 = tel2;}
+		else {throw new IllegalArgumentException("invalid tel2");}
 	}
-	//BirthDate
-	public LocalDate getBirthDate() {
-		return birthDate;
+	//Email
+	public String getEmailAccount() {
+		return emailAccount;
 	}
-	public void setBirthDate(LocalDate birthDate){
-		if(birthDate != null) {this.birthDate = birthDate;}
-		else {throw new IllegalArgumentException("invalid birthDate");}
+	public void setEmailAccount(String emailAccount) {
+		if(emailAccount != null && !emailAccount.isBlank()) {this.emailAccount = emailAccount;}
+		else {throw new IllegalArgumentException("invalid emailAccount");}
+
 	}
+	// If IsManager = true this class is the press officer 
+	public boolean isManager() {
+		return manager;
+	}
+	public void setManager(boolean manager) {
+		this.manager = manager;
+	}
+	
 	
 	
 }
--- POO222NotAEspanha/src/moduleEspanha/TechnicalCommitteeMember.java
+++ POO222NotAEspanha/src/moduleEspanha/TechnicalCommitteeMember.java
@@ -1,7 +1,49 @@
 package moduleEspanha;
 
 import java.io.Serializable;
+import java.time.LocalDate;
 
 public class TechnicalCommitteeMember extends Person implements Serializable {
-
+	//Atributos 
+		private String nickname;
+		private String role;
+		private LocalDate birthDate;
+		
+		//Constructors
+		public TechnicalCommitteeMember() {
+			super();
+		}
+		public TechnicalCommitteeMember(String name,String nickname, String role, LocalDate birthDate) {
+			super(name);
+			this.setBirthDate(birthDate);
+			this.setNickname(nickname);
+			this.setRole(role);
+		}
+		
+		//Nickname
+		public String getNickname() {
+			return nickname;
+		}
+		public void setNickname(String nickname) {
+			if(nickname != null && !nickname.isBlank()) {this.nickname = nickname;}
+			else {throw new IllegalArgumentException("invalid nickname");}
+		}
+		//Role 
+		public String getRole() {
+			return role;
+		}
+		public void setRole(String role) {
+			if(role != null && !role.isBlank()) {this.role = role;}
+			else {throw new IllegalArgumentException("invalid role");}
+			
+		}
+		//BirthDate
+		public LocalDate getBirthDate() {
+			return birthDate;
+		}
+		public void setBirthDate(LocalDate birthDate){
+			if(birthDate != null) {this.birthDate = birthDate;}
+			else {throw new IllegalArgumentException("invalid birthDate");}
+		}
+		
 }
